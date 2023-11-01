bc8b7929770db937de9e640e2ecab15a3af6560f
Biasiolo <biasiolorafael@gmail.com>
2023-11-01 08:33:52 -0300

    Feat: adaptação do código para plugin

--- main.js
+++ main.js
@@ -1,94 +1,130 @@
 $(document).ready(function() {
-    // Espera até que o documento esteja totalmente carregado
-
-    // Quando o botão "ADICIONAR IMAGEM" no cabeçalho é clicado, o formulário desliza para cima ou para baixo
-    $('header button').click(function() {
-        toggleForm();
+    // Inicializa o plugin e tenta carregar imagens a partir de cookies
+    $('body').imageGalleryPlugin({
+        loadImagesFromCookies: true
     });
-
-    // Quando o botão de cancelar é clicado, o formulário desliza para cima
-    $('#botao-cancelar').click(function() {
-        hideForm();
-    });
-
-    // Array para armazenar endereços
-    const enderecos = [];
-
-    $('form').on('submit', function(e) {
-        e.preventDefault();
-        const endNovaImagem = $('#end-imagem').val();
-        addImage(endNovaImagem);
-        // Adicione o endereço à lista de endereços
-        enderecos.push(endNovaImagem);
-    });
-
-    // Quando o botão "Salvar Endereços" é clicado, exibe os endereços no console (você pode personalizar esta parte)
-    $('#salvar-enderecos').click(function() {
-        if (enderecos.length === 0) {
-            alert('Nenhum endereço para salvar.');
-            return;
-        }
-
-        saveAddresses(enderecos);
-    });
-
-    function toggleForm() {
-        $('form').slideToggle(500); // Animação de deslizamento de 500 milissegundos (0,5 segundos)
-    }
-
-    function hideForm() {
-        $('form').slideUp(500); // Animação de deslizamento de 500 milissegundos (0,5 segundos)
-    }
-
-    function addImage(url) {
-        const novoItem = $('<li style="display: none;"></li>'); // Define o estilo 'display: none;'
-        $(`<img src="${url}" alt="Nova Imagem" />`).appendTo(novoItem);
-        $(`<div class="overlay-imagem"><a href="${url}" target="_blank" title="Ver imagem ampliada">Ver imagem ampliada</a></div>`).appendTo(novoItem);
-        $('ul').append(novoItem);
-        novoItem.fadeIn(1000); // Aplicar fadeIn para o novo item com 1000 milissegundos (1 segundo)
-        $('form')[0].reset(); // Limpa o formulário
-    }
-
-    function saveAddresses(addresses) {
-        const texto = addresses.join('\n'); // Converte o array de endereços em uma string com quebras de linha
-        const blob = new Blob([texto], { type: 'text/plain' });
-        const url = URL.createObjectURL(blob);
-
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'enderecos.txt'; // Nome do arquivo a ser baixado
-        a.style.display = 'none';
-        document.body.appendChild(a);
-
-        a.click();
-
-        document.body.removeChild(a);
-        URL.revokeObjectURL(url);
-
-        showSuccessAlert();
-    }
-
-    function showSuccessAlert() {
-        // Crie um elemento para o alerta e defina o estilo para centralizá-lo
-        const alertDiv = document.createElement('div');
-        alertDiv.style.position = 'fixed';
-        alertDiv.style.top = '50%';
-        alertDiv.style.left = '50%';
-        alertDiv.style.transform = 'translate(-50%, -50%)';
-        alertDiv.style.background = '#007ACC';
-        alertDiv.style.color = '#FFFFFF';
-        alertDiv.style.padding = '10px';
-        alertDiv.style.borderRadius = '8px';
-
-        // Texto do alerta
-        alertDiv.innerHTML = 'URLs salvas com sucesso!';
-
-        // Adicione o alerta ao corpo do documento
-        document.body.appendChild(alertDiv);
-
-        // Remova o alerta após alguns segundos (por exemplo, 3 segundos)
-        setTimeout(function() {
-            document.body.removeChild(alertDiv);
-        }, 3000); // 3000 milissegundos (3 segundos)
-    }
 });
+
+(function($) {
+    $.fn.imageGalleryPlugin = function(options) {
+        var defaults = {
+            animationDuration: 500,
+            loadImagesFromCookies: false
+        };
+
+        var settings = $.extend({}, defaults, options);
+
+        this.each(function() {
+            var $this = $(this);
+            var $form = $this.find('form');
+            var $imageList = $this.find('ul');
+            var $addButton = $this.find('header button');
+            var $cancelButton = $this.find('#botao-cancelar');
+            var $saveButton = $this.find('#salvar-enderecos');
+            var $addressInput = $this.find('#end-imagem');
+            var addresses = [];
+
+            $addButton.click(function() {
+                $form.slideToggle(settings.animationDuration);
+            });
+
+            $cancelButton.click(function() {
+                hideForm();
+            });
+
+            $form.on('submit', function(e) {
+                e.preventDefault();
+                var newImageAddress = $addressInput.val();
+                addImage(newImageAddress);
+                addresses.push(newImageAddress);
+                $addressInput.val('');
+
+                // Salva as imagens em cookies
+                if (settings.loadImagesFromCookies === true) {
+                    saveImagesToCookies(addresses);
+                }
+            });
+
+            $saveButton.click(function() {
+                if (addresses.length === 0) {
+                    alert('Nenhum endereço para salvar.');
+                } else {
+                    saveAddresses(addresses);
+                }
+            });
+
+            // Função para esconder o formulário
+            function hideForm() {
+                $form.slideUp(settings.animationDuration);
+            }
+
+            // Função para adicionar imagens
+            function addImage(url) {
+                var $newItem = $('<li style="display: none;"></li>');
+                $('<img src="' + url + '" alt="Nova Imagem" />').appendTo($newItem);
+                $('<div class="overlay-imagem"><a href="' + url + '" target="_blank" title="Ver imagem ampliada">Ver imagem ampliada</a></div>').appendTo($newItem);
+                $imageList.append($newItem);
+                $newItem.fadeIn(1000);
+            }
+
+            // Função para salvar imagens em cookies
+            function saveImagesToCookies(addresses) {
+                var serializedAddresses = JSON.stringify(addresses);
+                document.cookie = 'imageAddresses=' + serializedAddresses;
+            }
+
+            // Função para carregar imagens de cookies
+            function loadImagesFromCookies() {
+                var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)imageAddresses\s*=\s*([^;]*).*$)|^.*$/, '$1');
+                if (cookieValue) {
+                    var addressesFromCookies = JSON.parse(cookieValue);
+                    addressesFromCookies.forEach(function(address) {
+                        addImage(address);
+                        addresses.push(address);
+                    });
+                }
+            }
+
+            // Função para salvar endereços em um arquivo
+            function saveAddresses(addresses) {
+                var text = addresses.join('\n');
+                var blob = new Blob([text], { type: 'text/plain' });
+                var url = URL.createObjectURL(blob);
+                var a = document.createElement('a');
+                a.href = url;
+                a.download = 'enderecos.txt';
+                a.style.display = 'none';
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+                URL.revokeObjectURL(url);
+                showSuccessAlert();
+            }
+
+            // Função para exibir um alerta de sucesso
+            function showSuccessAlert() {
+                var alertDiv = $('<div></div>');
+                alertDiv.css({
+                    position: 'fixed',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    background: '#007ACC',
+                    color: '#FFFFFF',
+                    padding: '10px',
+                    borderRadius: '8px',
+                });
+                alertDiv.text('URLs salvas com sucesso!');
+                document.body.appendChild(alertDiv[0]);
+                setTimeout(function() {
+                    alertDiv.remove();
+                }, 3000);
+            }
+
+            // Carrega imagens a partir de cookies, se necessário
+            if (settings.loadImagesFromCookies === true) {
+                loadImagesFromCookies();
+            }
+        });
+    };
+})(jQuery);
